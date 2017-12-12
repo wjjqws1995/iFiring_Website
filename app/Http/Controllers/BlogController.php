58fa26e5aa89d7f6816f7868ba87801ae1bdb137
iFiring <wchuan@drips.cn>
2017-12-12 23:23:46 +0800

    create BlogController

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DummyFullModelClass;
+use App\lain;
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+use Carbon\Carbon;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\lain  $lain
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $posts = Post::where('published_at', '<=', Carbon::now())
+                ->orderBy('published_at','desc')
+                ->paginate(config('blog.posts_per_page'));
+
+        return view('blog.index',compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \App\lain  $lain
+     * @return \Illuminate\Http\Response
+     */
+    public function showPost($slug){
+        $post = Post::shereSlug($slug)->firstOrFail();
+        return view('blog.post')->withPost($post);
+    }
+    public function create(lain $lain)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\lain  $lain
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, lain $lain)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+}
