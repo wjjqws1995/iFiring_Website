9752332e1570b7ec720adc4af01346bf4ae0da4e
iFiring <wchuan@drips.cn>
2017-12-15 16:51:43 +0800

    Add UploadController

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class \UploadController extends Controller
+class UploadController extends Controller
 {
     //
+
 }
