d78d9bba8d6e63afba92afe913753aa11eee1a3e
iFiring <wchuan@drips.cn>
2017-12-15 19:08:31 +0800

    update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                         @include('admin.partials.errors')
 
                         <form class="form-horizontal" role="form" method="POST"
-                              action="{{ url('/login') }}">
+                              action="{{ url('/auth/login') }}">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                             <div class="form-group">
