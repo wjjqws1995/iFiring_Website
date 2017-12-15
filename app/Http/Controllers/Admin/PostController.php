aa556d5786c2f7847aee0ae4923dcd5ba1fc68c3
iFiring <wchuan@drips.cn>
2017-12-15 16:50:02 +0800

    add PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class \PostController extends Controller
+class PostController extends Controller
 {
     //
+    public function index(){
+        return view('admin.post.index');
+    }
 }
