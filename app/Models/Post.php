04ce79d14addda848c6b91802abfaad813c2fe42
iFiring <wchuan@drips.cn>
2017-12-17 22:41:50 +0800

    Update Post Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,22 +2,105 @@
 
 namespace App\Models;
 
+use App\Services\Markdowner;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     //
-    //protected $dates = ['published_at'];
+    protected $dates = ['published_at'];
+    protected $fillable = [
+        'title', 'subtitle', 'content_raw', 'page_image', 'meta_description','layout', 'is_draft', 'published_at',
+    ];
+    /**
+     * The many-to-many relationship between posts and tags.
+     *
+     * @return BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag', 'post_tag_pivot');
+    }
 
     /**
-     * @param $value
+     * Set the title attribute and automatically the slug
+     *
+     * @param string $value
      */
     public function setTitleAttribute($value){
 
         $this->attributes['title'] = $value;
 
         if(!$this->exists){
-            $this->attributes['slug'] = str_slug($value);
+            $this->setUniqueSlug($value, '');
+            //$this->attributes['slug'] = str_slug($value);
         }
+    }
+    protected function setUniqueSlug($title, $extra)
+    {
+        $slug = str_slug($title.'-'.$extra);
+
+        if (static::whereSlug($slug)->exists()) {
+            $this->setUniqueSlug($title, $extra + 1);
+            return;
+        }
+
+        $this->attributes['slug'] = $slug;
+    }
+
+    /**
+     * Set the HTML content automatically when the raw content is set
+     *
+     * @param string $value
+     */
+    public function setContentRawAttribute($value)
+    {
+        $markdown = new Markdowner();
+
+        $this->attributes['content_raw'] = $value;
+        $this->attributes['content_html'] = $markdown->toHTML($value);
+    }
+
+    /**
+     * Sync tag relation adding new tags as needed
+     *
+     * @param array $tags
+     */
+    public function syncTags(array $tags)
+    {
+        Tag::addNeededTags($tags);
+
+        if (count($tags)) {
+            $this->tags()->sync(
+                Tag::whereIn('tag', $tags)->lists('id')->all()
+            );
+            return;
+        }
+
+        $this->tags()->detach();
+    }
+
+    /**
+     * Return the date portion of published_at
+     */
+    public function getPublishDateAttribute($value)
+    {
+        return $this->published_at->format('M-j-Y');
+    }
+
+    /**
+     * Return the time portion of published_at
+     */
+    public function getPublishTimeAttribute($value)
+    {
+        return $this->published_at->format('g:i A');
+    }
+
+    /**
+     * 作为访问器以便返回 $this->content_raw,使用 $post->content  就会执行该方法。
+     */
+    public function getContentAttribute($value)
+    {
+        return $this->content_raw;
     }
 }
